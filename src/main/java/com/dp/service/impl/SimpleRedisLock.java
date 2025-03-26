998aa3b3ca3224d1132ecab4e52f0cac690724ad
YWX <930924708@qq.com>
2025-03-26 16:15:34 +0800

    refactor(redis锁): 使用Lua脚本实现原子性解锁操作
    为了确保解锁操作的原子性，避免在高并发场景下出现竞争条件，将原有的Java代码解锁逻辑替换为Lua脚本。Lua脚本在Redis中执行时是原子性的，能够保证在获取锁标识和删除锁的操作之间不会被其他线程打断。

--- src/main/java/com/dp/service/impl/SimpleRedisLock.java
+++ src/main/java/com/dp/service/impl/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.dp.service.impl;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import com.dp.service.ILock;
 
@@ -16,6 +19,12 @@ public class SimpleRedisLock implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -33,13 +42,19 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // String threadId = ID_PREFIX + Thread.currentThread().getId();
 
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
+        // String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // if (threadId.equals(id)) {
+        //     stringRedisTemplate.delete(KEY_PREFIX + name);
 
-        }
+        // }
+
+        stringRedisTemplate.execute(
+            UNLOCK_SCRIPT,
+            Collections.singletonList(KEY_PREFIX + name),
+            ID_PREFIX + Thread.currentThread().getId()
+        );
     }
 
 }
