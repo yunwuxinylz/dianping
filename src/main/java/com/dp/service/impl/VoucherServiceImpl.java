8dfcd4468c00f54d1fd54a878614b84cbd88232b
YWX <930924708@qq.com>
2025-04-01 15:37:44 +0800

    feat: 实现秒杀订单异步处理及库存管理
    引入Lua脚本处理秒杀逻辑，优化库存管理，新增异步线程处理订单，提升系统性能与并发处理能力。修改`createVoucherOrder`方法以支持异步处理，并调整相关Redis配置。

--- src/main/java/com/dp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/dp/service/impl/VoucherServiceImpl.java
@@ -7,10 +7,15 @@ import com.dp.mapper.VoucherMapper;
 import com.dp.entity.SeckillVoucher;
 import com.dp.service.ISeckillVoucherService;
 import com.dp.service.IVoucherService;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+
+import static com.dp.utils.RedisConstants.SECKILL_STOCK_KEY;
+
 import java.util.List;
 
 /**
@@ -25,6 +30,9 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
 
     @Resource
     private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Result queryVoucherOfShop(Long shopId) {
@@ -46,5 +54,7 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         seckillVoucher.setBeginTime(voucher.getBeginTime());
         seckillVoucher.setEndTime(voucher.getEndTime());
         seckillVoucherService.save(seckillVoucher);
+        // 保存秒杀库存到redis
+        stringRedisTemplate.opsForValue().set(SECKILL_STOCK_KEY + voucher.getId(), voucher.getStock().toString());
     }
 }
