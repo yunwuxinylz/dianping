a9fe5b6ca82198d46f5ed5220fed7d44452565a2
YWX <930924708@qq.com>
2025-05-19 18:33:20 +0800

    优化MvcConfig中的拦截器配置，调整代码格式以提升可读性

--- src/main/java/com/dp/config/MvcConfig.java
+++ src/main/java/com/dp/config/MvcConfig.java
@@ -3,7 +3,6 @@ package com.dp.config;
 import javax.annotation.Resource;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,29 +17,29 @@ import com.dp.utils.RefreshTokenInterceptor;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    private JwtUtils jwtUtils;
+        @Resource
+        private JwtUtils jwtUtils;
 
-    @Override
-    public void addInterceptors(@SuppressWarnings("null") InterceptorRegistry registry) {
-        // 登录拦截器
-        registry.addInterceptor(new LoginInterceptor())
-                .excludePathPatterns(
-                        "/shop/**",
-                        "/voucher/**",
-                        "/upload/**",
-                        "/blog/hot",
-                        "/user/code",
-                        "/user/login",
-                        "/user/logout",
-                        "/user/register",
-                        "/user/reset-password",
-                        "/user/refresh-token",
-                        "/goods/**",
-                        "/favorite/**")
-                .order(1);
-        // token刷新的拦截器
-        registry.addInterceptor(new RefreshTokenInterceptor(jwtUtils)).addPathPatterns("/**")
-                .order(0);
-    }
+        @Override
+        public void addInterceptors(@SuppressWarnings("null") InterceptorRegistry registry) {
+                // 登录拦截器
+                registry.addInterceptor(new LoginInterceptor())
+                                .excludePathPatterns(
+                                                "/shop/**",
+                                                "/voucher/**",
+                                                "/upload/**",
+                                                "/blog/hot",
+                                                "/user/code",
+                                                "/user/login",
+                                                "/user/logout",
+                                                "/user/register",
+                                                "/user/reset-password",
+                                                "/user/refresh-token",
+                                                "/goods/**",
+                                                "/favorite/**")
+                                .order(1);
+                // token刷新的拦截器
+                registry.addInterceptor(new RefreshTokenInterceptor(jwtUtils)).addPathPatterns("/**")
+                                .order(0);
+        }
 }
